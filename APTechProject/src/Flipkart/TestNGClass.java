d7de3da3b7ea74d01a03072a42c0ec947013e1d2
user <user@DESKTOP-TQF3AML>
2021-12-14 09:58:12 -0800

    Test case was desabled by enabled parameter

--- APTechProject/src/Flipkart/TestNGClass.java
+++ APTechProject/src/Flipkart/TestNGClass.java
@@ -5,10 +5,16 @@ import org.testng.annotations.Test;
 public class TestNGClass {
 	
 	
-	@Test
+	@Test(enabled=false)
 	public void login() {
 		System.out.println("Hii");
 		System.out.println("Hello");
+		
+		
+	}
+	
+	@Test
+	public void Register() {
 		System.out.println("How are you");
 		
 	}
