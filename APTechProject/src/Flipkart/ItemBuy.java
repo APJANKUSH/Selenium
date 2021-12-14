0c2ba97da4cad1651a6bee9156b84f8a472c6a8a
user <user@DESKTOP-TQF3AML>
2021-12-14 09:51:43 -0800

    TestNg class was addes and XML file was created successfully

--- APTechProject/src/Flipkart/ItemBuy.java
+++ APTechProject/src/Flipkart/ItemBuy.java
@@ -3,12 +3,13 @@ package Flipkart;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Login {
+public class ItemBuy {
 
+		
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Desktop\\APJ\\Selenium\\Drivers-or-jar\\Chrome\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		//Browser Script
@@ -27,28 +28,9 @@ public class Login {
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input")).sendKeys("1529@Mypassword");
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[4]/button")).click();
-        //Forget Password 
-//        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/a")).click();
-//        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[4]/button")).click();
-//        
-	    //Item Search
-        Thread.sleep(1000);
-		driver.findElement(By.className("_3704LK")).sendKeys("Readmi 9A");
-		driver.findElement(By.className("_34RNph")).click();
-		
-		//select item 
-		Thread.sleep(1000);
-	    //driver.findElement(By.xpath("//div[contains(text(),'Redmi 9A (SeaBlue, 32 GB)')]")).click();
-		driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[2]/div[3]/div/div/div/a/div[2]/div[1]/div[1]")).click();
-	    
-	    //Add item to card
-		//driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[2]/form/button")).click();
-		//driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[2]/div[6]/div/div/div[1]/div[2]/div/div[2]/div/span/div/div")).click();
-		driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[1]/button")).click();
-		
-//	    Thread.sleep(1000);
-	    driver.close();
-	    
+        
+        driver.navigate().refresh();
+
 	}
 
 }
---
+++ APTechProject/src/Flipkart/TestNGClass.java
@@ -0,0 +1,16 @@
+package Flipkart;
+
+import org.testng.annotations.Test;
+
+public class TestNGClass {
+	
+	
+	@Test
+	public void login() {
+		System.out.println("Hii");
+		System.out.println("Hello");
+		System.out.println("How are you");
+		
+	}
+
+}
